3b7a4e15d49d2e76d12100c580e5c692b3921d03
jaeel29 <elferkhjaber@gmail.com>
2021-01-14 11:21:17 +0100

    finale commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -29,7 +29,7 @@ function Login() {
           src="https://www.exchangewire.com/wp-content/uploads/2017/04/snap-ghost-yellow-300x300-271x271.png"
           alt=""
         />
-        <Button onClick={signin} variant="outline">
+        <Button onClick={signin} variant="outlined">
           Sign In
         </Button>
       </div>
